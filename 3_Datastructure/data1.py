0b32a4d2eac0a4cf36eb159c9713aef93fb7359e
sunilsala88 <sunilsala88@gmail.com>
2025-07-27 15:11:15 +0530

    jul-27-2

--- 3_Datastructure/data1.py
+++ 3_Datastructure/data1.py
@@ -1,1 +1,17 @@
-print('hello world')
+
+
+l1=['tsla','goog','amzn']
+print(l1[0])
+print(l1[1:2])
+l1.append('nvda')
+print(l1)
+a=l1.remove('goog')
+print(a)
+print(l1)
+l1.insert(1,'nifty')
+print(l1)
+a=l1.pop(0)
+print(a)
+print(l1) 
+i=l1.index('nifty')
+print(i)
