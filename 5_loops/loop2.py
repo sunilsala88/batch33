e6bc18dfdc3645b5d830cfa920f3a200de9f1380
sunilsala88 <sunilsala88@gmail.com>
2025-08-03 15:26:34 +0530

    aug-3-5

--- 5_loops/loop2.py
+++ 5_loops/loop2.py
@@ -7,3 +7,13 @@ for i in l1:
         high=i
 
 print(high)
+
+pnl= [+10, -20, +15, +30, -5]
+count=0
+for i in pnl:
+    if i>0:
+        count=count+1
+
+print(count)
+
+prices=[100, 120, 130, 140, 150]
