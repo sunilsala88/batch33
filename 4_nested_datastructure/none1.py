f6d6622f811d59aeab212ab30d96a41c8972aa75
sunilsala88 <sunilsala88@gmail.com>
2025-08-03 14:45:25 +0530

    aug-3-1

---
+++ 4_nested_datastructure/none1.py
@@ -0,0 +1,19 @@
+
+
+data={"ticker": "TSLA", "quantity": 5000, "average_buy_price": 600}
+print(data)
+v=data.get('quantitys')
+print(v)
+
+a=None
+print(type(a))
+
+num=10
+if []:
+    print('inside if')
+else:
+    print('insdie else')
+
+#truthy and flasy value
+#flasy values
+#0 "" [],{},(),None,False
---
+++ 5_loops/loop1.py
@@ -0,0 +1,15 @@
+
+
+l1=[44,55,66,77,88]
+
+#iterate/looping
+total=0
+
+for p in l1:
+    # print(p)
+    total=total+p
+
+print(total)
+n=len(l1)
+print(n)
+print('average',total/n)
