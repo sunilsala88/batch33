fc7664c3caba7b4f929b92772b035d8693b72000
sunilsala88 <sunilsala88@gmail.com>
2025-08-03 15:00:22 +0530

    aug-3-2

--- 5_loops/loop1.py
+++ 5_loops/loop1.py
@@ -6,10 +6,27 @@ l1=[44,55,66,77,88]
 total=0
 
 for p in l1:
-    # print(p)
     total=total+p
 
 print(total)
 n=len(l1)
 print(n)
 print('average',total/n)
+
+#type 1
+for i in l1:
+    print(i)
+
+#type 2
+a=list(range(100))
+print(a)
+for i in range(10):
+    print('hello world')
+
+#type 3 
+l2=[5,6,78,89,33,85,78,67]
+for i in range(len(l2)):
+    print(l2[i])
+
+#square all the elem of the list
+list3=[2,3,4,5]
