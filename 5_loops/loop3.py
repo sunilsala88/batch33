e111c44ffdf57a862d688f1412f42db41b075154
sunilsala88 <sunilsala88@gmail.com>
2025-08-03 16:33:24 +0530

    aug3-8

--- 5_loops/loop3.py
+++ 5_loops/loop3.py
@@ -12,3 +12,16 @@ for i,j in data.items():
     print(i,j)
 
 stock_prices={'tsla':500,'nvda':400,'amzn':890}
+sum1=0
+for i,j in stock_prices.items():
+    sum1=sum1+j
+
+print(sum1)
+large=0
+large_key=0
+for i,j in stock_prices.items():
+    if j>large:
+        large=j
+        large_key=i
+
+print(large_key,large)
