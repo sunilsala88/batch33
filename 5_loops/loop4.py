8abb8b4bbaec204eb4b0c6f13560d6d9c082a920
sunilsala88 <sunilsala88@gmail.com>
2025-08-09 14:45:10 +0530

    aug-9-1

---
+++ 5_loops/loop4.py
@@ -0,0 +1,36 @@
+
+
+#type1
+list1=[44,55,66,77,88]
+for i in list1:
+    print(i)
+
+#type2
+
+for i in range(10):
+    print('hello')
+
+#type3
+
+for i in range(len(list1)):
+    print(list1[i])
+
+#type 4
+stock_prices={'tlsa':500,'goog':900,'amzn':567}
+for i,j in stock_prices.items():
+    print(i,j)
+
+num=100
+total=0
+for i in range(num+1):
+    total=total+i
+
+print(total)
+
+print(list(range(1,101)))
+total=0
+for i in range(1,num+1):
+    total=total+i
+print(total)
+
+#1 1 2 3 5 8 13 21 34 55 ...
