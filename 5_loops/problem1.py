fe81be8548d98b94009a766279bbd8acd6b5eca6
sunilsala88 <sunilsala88@gmail.com>
2025-08-10 15:21:53 +0530

    aug-10-4

--- 5_loops/problem1.py
+++ 5_loops/problem1.py
@@ -1,6 +1,10 @@
 
 
 stock_prices={'tsla':500,'goog':700,'reliance':678,'nifty':456,'nvda':980}
+
+for i,j in stock_prices.items():
+    print(i,":",j)
+
 portfolio=[]
 
 while True:
@@ -8,6 +12,9 @@ while True:
     print(name)
     if name.lower()=='q':
         break
+    if name=='nvda':
+        print('you cannot trade this stock try something else')
+        continue
 
     found=stock_prices.get(name)
     if found:
@@ -16,3 +23,5 @@ while True:
         print('this stock does not exist type again')
 
 print(portfolio)
+
+#generate a list of all num from 1 to 100 but no num div by 5
