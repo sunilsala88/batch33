ce6116836f46e271e5052c9abd97ffd252a121e7
sunilsala88 <sunilsala88@gmail.com>
2025-08-10 15:42:00 +0530

    aug10-5

--- 5_loops/problem1.py
+++ 5_loops/problem1.py
@@ -1,27 +1,54 @@
 
 
-stock_prices={'tsla':500,'goog':700,'reliance':678,'nifty':456,'nvda':980}
+# stock_prices={'tsla':500,'goog':700,'reliance':678,'nifty':456,'nvda':980}
 
-for i,j in stock_prices.items():
-    print(i,":",j)
+# for i,j in stock_prices.items():
+#     print(i,":",j)
 
-portfolio=[]
+# portfolio=[]
 
-while True:
-    name=input('enter the stock to buy (q to quit)')
-    print(name)
-    if name.lower()=='q':
-        break
-    if name=='nvda':
-        print('you cannot trade this stock try something else')
-        continue
+# while True:
+#     name=input('enter the stock to buy (q to quit)')
+#     print(name)
+#     if name.lower()=='q':
+#         break
+#     if name=='nvda':
+#         print('you cannot trade this stock try something else')
+#         continue
 
-    found=stock_prices.get(name)
-    if found:
-        portfolio.append(name)
-    else:
-        print('this stock does not exist type again')
+#     found=stock_prices.get(name)
+#     if found:
+#         portfolio.append(name)
+#     else:
+#         print('this stock does not exist type again')
 
-print(portfolio)
+# print(portfolio)
 
 #generate a list of all num from 1 to 100 but no num div by 5
+
+# i=0
+# l1=[]
+# while True:
+#     if i==100:
+#         break
+
+#     print(i)
+#     i =i+1
+#     if i%5==0:
+#         continue
+#     l1.append(i)
+
+
+# print(l1)
+
+capital=1000
+interest=0.08
+current_capital=capital
+year=0
+while True:
+    if current_capital>2*capital:
+        break
+    current_capital=current_capital+(current_capital*interest)
+    year=year+1
+
+print(year)
