dc99474ef9f0e47391396e6700f07a013e89647c
sunilsala88 <sunilsala88@gmail.com>
2025-08-16 15:37:30 +0530

    aug16-3

--- 6_function/fun2.py
+++ 6_function/fun2.py
@@ -10,3 +10,18 @@ def add(num1 :str ,num2 :str)-> str:
 
 a=add('10','2')
 print(a)
+
+
+def factorial(num:int)->list:
+
+    fact=num
+    while True:
+        num=num-1
+        if num==0:
+            break
+
+        fact=fact*num
+    return fact
+
+n=factorial(5)
+print(n)
