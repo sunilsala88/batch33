e5e162cb24d26ffeefb873c9f8be1ee7648cd65c
sunilsala88 <sunilsala88@gmail.com>
2025-08-17 15:50:59 +0530

    aug-17-5

---
+++ 7_files_exception/problem1.py
@@ -0,0 +1,36 @@
+
+
+stock_prices={'tsla':500,'goog':700,'reliance':678,'nifty':456,'nvda':980}
+
+def print_stocks():
+    for i,j in stock_prices.items():
+        print(i,":",j)
+
+
+def buy_stocks():
+    porfolio={}
+
+    while True:
+        name=input('enter the name of stock (q to quit)')
+        if name.lower()=='q':
+            break
+        found=stock_prices.get(name)
+        if found:
+            porfolio.update({name:found})
+        else:
+            print('this stock does not exist try again')
+    return porfolio
+
+def save_data(port):
+
+    f1=open('data.txt','w')
+    for i,j in port.items():
+        d1=i+':'+str(j)+'\n'
+        f1.write(d1)
+    
+    f1.close()
+
+print_stocks()
+p=buy_stocks()
+save_data(p)
+
