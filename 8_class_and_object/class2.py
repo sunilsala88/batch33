de68b400225fc1c93c7437f007f77dde44c61cb7
sunilsala88 <sunilsala88@gmail.com>
2025-08-24 14:54:26 +0530

    aug-24-2

--- 8_class_and_object/class2.py
+++ 8_class_and_object/class2.py
@@ -18,12 +18,32 @@ class Broker:
         return self.portfolio
     
     def buy_stock(self,stock_name):
-        pass
+        found=self.stock_prices.get(stock_name)
+        if found:
+            
+            if self.wallet>found:
+                self.portfolio.update({stock_name:found})
+                self.wallet=self.wallet-found
+            else:
+                print('you dont have enough money to buy')
+        else:
+            print('this stock does not exist')
 
     def sell_stock(self,stock_name):
-        pass
+        found=self.portfolio.get(stock_name)
+        if found:
+            self.portfolio.pop(stock_name)
+            self.wallet=self.wallet-found
+        else:
+            print('this stock is not in your porfolio')
 
-u1=Broker('matt',568,1000)
+u1=Broker('matt',568,3000)
 u2=Broker('rock',569,2000)
 print(u2)
 
+u1.buy_stock('tsla')
+print(u1.get_portfolio())
+u1.buy_stock('amzn')
+print(u1.get_portfolio())
+u1.sell_stock('amzn')
+print(u1.get_portfolio())
