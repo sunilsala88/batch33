601b8f1857d436c16cd7a54a2c380dfbaacf7cd0
sunilsala88 <sunilsala88@gmail.com>
2025-08-30 15:50:49 +0530

    aug-30-4

--- 10_data_analysis/nump1.py
+++ 10_data_analysis/nump1.py
@@ -4,7 +4,25 @@
 
 import numpy as np
 
-l1=[33,44,55,66,7.7]
-print(l1)
+# l1=[33,44,55,66,7.7]
+# print(l1)
+# np1=np.array(l1)
+# print(np1)
+
+# n2=np.arange(10)
+# print(n2)
+
+
+
+l1=[1,2,3,4]
+l2=[5,6,7,8]
+print(l1+l2)
+
 np1=np.array(l1)
-print(np1)
+np2=np.array(l2)
+print(np1%np2)
+
+z1=np.zeros(10)
+print(z1)
+z2=np.ones(20)
+print(z2)
