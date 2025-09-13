07769b4517ddf68c24b7393017a739c2733ae07e
sunilsala88 <sunilsala88@gmail.com>
2025-09-13 15:50:15 +0530

    sep13-5

--- 11_datetime_analysis/date2.py
+++ 11_datetime_analysis/date2.py
@@ -13,3 +13,13 @@ for i in data['date']:
     l1.append(dt.datetime.strptime(i,'%Y-%m-%d %H:%M:%S'))
 data['date']=l1
 print(data.info())
+
+dt1=dt.datetime.now()
+print(dt1.time())
+data['time1']=data['date'].dt.time
+data['time1']=data['time1'].astype('str')
+
+print(data)
+print(data.info())
+print(data[data['time1']=='09:15:00']['close'].to_list())
+print(data.iloc[0,-1]=='09:15:00')
