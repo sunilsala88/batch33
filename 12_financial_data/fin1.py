1aa5fd56b2691e68a45e9fe553a71da48c17b222
sunilsala88 <sunilsala88@gmail.com>
2025-09-14 15:19:43 +0530

    sep-14-2

--- 12_financial_data/fin1.py
+++ 12_financial_data/fin1.py
@@ -1,5 +1,10 @@
 
 import yfinance as yf
 
-data=yf.download(tickers='NVDA',period='6mo')
+# data=yf.download(tickers='TSLA',period='6d',interval='1m',multi_level_index=False,ignore_tz=True)
+# print(data)
+
+
+data=yf.download(tickers='TSLA',start='2024-01-01',end='2024-12-31',interval='1h',multi_level_index=False)
 print(data)
+data.to_csv('stock_data.csv')
