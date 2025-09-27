a0fe198cf2222fa2d5b71012057d0f7355c79bc1
sunilsala88 <sunilsala88@gmail.com>
2025-09-27 16:43:19 +0530

    sep-27-1

--- 14_technical/tech1.py
+++ 14_technical/tech1.py
@@ -4,4 +4,11 @@ data=yf.download('TSLA',period='10y',multi_level_index=False)
 print(data)
 data['sma']=data['Close'].rolling(window=10).mean()
 
+
+import pandas_ta as ta
+data['sma2']=ta.sma(data['Close'],length=10)
+
+data['ema']=ta.ema(data['Close'],length=10)
+#pandasta
+#talib
 print(data)
