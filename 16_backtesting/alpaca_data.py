b7788e7a1e55f0b8889d8aa88ae1c94ea8d372d8
sunilsala88 <sunilsala88@gmail.com>
2025-11-02 17:06:22 +0530

    nov-2-3

--- 16_backtesting/alpaca_data.py
+++ 16_backtesting/alpaca_data.py
@@ -13,35 +13,35 @@ current_time=dt.now(tz=timez_zone)
 print(current_time)
 import time
 
-# ct=time.time()
-# req = StockBarsRequest(
-#     symbol_or_symbols = 'MSFT',
-#     timeframe=TimeFrame(amount = 1, unit = TimeFrameUnit.Hour), # specify timeframe
-#     start = current_time-dt.duration(days=1000),                          # specify start datetime, default=the beginning of the current day.
-#     end_date=current_time,                                        # specify end datetime, default=now
-#     # limit = 2,                                               # specify limit
-# )
-
-# history1=stock_historical_data_client.get_stock_bars(req).df
-# history1.to_csv('data.csv')
-# print('time taken',time.time()-ct)
-
-
-from alpaca.data.historical.crypto import CryptoHistoricalDataClient
-from alpaca.data.requests import CryptoBarsRequest
-from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
-
-crypto_historical_data_client = CryptoHistoricalDataClient()
-req = CryptoBarsRequest(
-    symbol_or_symbols = 'ETH/USD',
+ct=time.time()
+req = StockBarsRequest(
+    symbol_or_symbols = 'MSFT',
     timeframe=TimeFrame(amount = 1, unit = TimeFrameUnit.Hour), # specify timeframe
-    start = dt.datetime(2024,1,1,1,1,1,tz=timez_zone),                          # specify start datetime, default=the beginning of the current day.
-    end=dt.datetime(2024,12,30,1,1,1,tz=timez_zone),                                        # specify end datetime, default=now
+    start = current_time-dt.duration(days=1000),                          # specify start datetime, default=the beginning of the current day.
+    end_date=current_time,                                        # specify end datetime, default=now
     # limit = 2,                                               # specify limit
 )
 
-history_df2=crypto_historical_data_client.get_crypto_bars(req).df
-history_df2
+history1=stock_historical_data_client.get_stock_bars(req).df
+history1.to_csv('data.csv')
+print('time taken',time.time()-ct)
 
-history_df2=stock_historical_data_client.get_stock_bars(req).df
-history_df2.to_csv('data.csv')
+
+# from alpaca.data.historical.crypto import CryptoHistoricalDataClient
+# from alpaca.data.requests import CryptoBarsRequest
+# from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
+
+# crypto_historical_data_client = CryptoHistoricalDataClient()
+# req = CryptoBarsRequest(
+#     symbol_or_symbols = 'ETH/USD',
+#     timeframe=TimeFrame(amount = 1, unit = TimeFrameUnit.Hour), # specify timeframe
+#     start = dt.datetime(2024,1,1,1,1,1,tz=timez_zone),                          # specify start datetime, default=the beginning of the current day.
+#     end=dt.datetime(2024,12,30,1,1,1,tz=timez_zone),                                        # specify end datetime, default=now
+#     # limit = 2,                                               # specify limit
+# )
+
+# history_df2=crypto_historical_data_client.get_crypto_bars(req).df
+# history_df2
+
+# history_df2=stock_historical_data_client.get_stock_bars(req).df
+# history_df2.to_csv('data.csv')
--- 16_backtesting/tp_sl.py
+++ 16_backtesting/tp_sl.py
@@ -185,3 +185,11 @@ bt=Backtest(data,Supertrend,cash=10000,finalize_trades=True)
 output=bt.run()
 print(output)
 bt.plot()
+
+
+
+
+output2=bt.optimize(sl1=[0.03,0.04,0.05,0.06,0.07,0.08,0.09],maximize='Return [%]')
+print(output2)
+print(output2['_strategy'])
+bt.plot()
